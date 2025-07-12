7f94bfd7cb9ab4dfaeba6aaad0d6156f8d9acac2
FabioMijango <00156022@uca.edu.sv>
2025-07-12 00:13:32 -0600

    Actualiza la ventana y el manejo de eventos
    Se cambia el título de la ventana a "Space Invaders" y se ajusta el límite de fotogramas a 60. Se añade la funcionalidad para cerrar la ventana con la tecla Escape y se implementa el ajuste automático del tamaño de la ventana si es menor a 720 píxeles, junto con la configuración de la vista visible.

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Space Invaders");
+    window.setFramerateLimit(60);
+
+
 
     while (window.isOpen())
     {
@@ -13,6 +15,26 @@ int main()
             {
                 window.close();
             }
+            else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+            {
+                if (keyPressed->scancode == sf::Keyboard::Scan::Escape)
+                {
+                    window.close();
+                }
+            }
+
+            if (const auto* resized = event->getIf<sf::Event::Resized>())
+            {
+                if (resized->size.x < 720) {
+                    window.setSize(sf::Vector2u(720u, resized->size.y));
+                }
+                if (resized->size.y < 720) {
+					window.setSize(sf::Vector2u(resized->size.y, 720u));
+                }
+                sf::FloatRect visibleArea(sf::Vector2f(0, 0), sf::Vector2f(resized->size.x, resized->size.y));
+                window.setView(sf::View(visibleArea));
+            }
+        
         }
 
         window.clear();
