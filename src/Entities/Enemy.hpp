b599e633ed59a1421a06fc28fd14ad8242cd6968
FabioMijango <00156022@uca.edu.sv>
2025-07-13 12:33:23 -0600

    Añadir clase Enemy con métodos de actualización y renderizado

---
+++ src/Entities/Enemy.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Entity.hpp"
+
+
+class Enemy : public Entity {
+public:
+    Enemy();
+
+    void update(float deltaTime) override;
+    void render(sf::RenderWindow& window) override;
+
+    sf::Vector2f getPosition() const override;
+    sf::FloatRect getBounds() const override;
+
+private:
+    sf::Sprite sprite;
+    sf::Vector2f direction;
+
+    int healt;
+    float velocity = 400.f;
+};
