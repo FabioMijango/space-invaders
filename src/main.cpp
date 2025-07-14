989027db1a9ae2456cf39b8cad0d888baa985df4
FabioMijango <00156022@uca.edu.sv>
2025-07-14 01:51:55 -0600

    ✨ Update Renderer to use level-specific enemies
    Changed the enemy retrieval method in the Renderer
    initialization to use getLevelEnemies for improved
    level management and organization.

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main()
 
 	EntityManager entityManager(resourceManager);
 
-    Renderer renderer(window, resourceManager, entityManager.getPlayer(), entityManager.getEnemies());
+    Renderer renderer(window, resourceManager, entityManager.getPlayer(), entityManager.getLevelEnemies());
 
     sf::Clock clock;
     float deltaTime = clock.restart().asSeconds();
