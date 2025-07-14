cad7572131019bd7872cc47698b1f0c0069a5570
FabioMijango <00156022@uca.edu.sv>
2025-07-14 01:49:03 -0600

    ✨ Add ENEMY_SPACING constant for enemy positioning
    Introduce a new constant for spacing between enemies to improve layout and positioning in the game.

--- src/Utils/Constants.hpp
+++ src/Utils/Constants.hpp
@@ -15,6 +15,7 @@
 #define ENEMY_QUANTITY 5
 #define ENEMY_HEIGHT 8
 #define ENEMY_WIDTH 11
+#define ENEMY_SPACING sf::Vector2f(10, 10)
 // Structure
 #define STRUCTURE_QUANTITY 4
 #define STRUCTURE_HEIGHT 12
