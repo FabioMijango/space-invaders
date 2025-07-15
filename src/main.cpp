05c45c50473583ee80c6da2dbb846a08e5bdeb25
FabioMijango <00156022@uca.edu.sv>
2025-07-15 14:25:48 -0600

    🎮 Update Renderer to include enemy death status
    Add enemy death status to Renderer initialization for improved rendering logic.

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main()
 
 	EntityManager entityManager(resourceManager);
 
-    Renderer renderer(window, resourceManager, entityManager.getPlayer(), entityManager.getLevelEnemies());
+    Renderer renderer(window, resourceManager, entityManager.getPlayer(), entityManager.getLevelEnemies(), entityManager.getIsEnemyDead());
 
     sf::Clock clock;
     float deltaTime = clock.restart().asSeconds();
