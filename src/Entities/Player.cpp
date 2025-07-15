cdd6c386b9ee0a18d7bc23529671d738e187c491
FabioMijango <00156022@uca.edu.sv>
2025-07-14 19:22:43 -0600

    🎮 Enhance Player class with shooting mechanics
    - Added shoot texture handling in Player constructor.
    - Implemented shooting logic in update method.
    - Added methods for shoot state management and rendering.

--- src/Entities/Player.cpp
+++ src/Entities/Player.cpp
@@ -1,11 +1,15 @@
 
 #include "Player.hpp"
 
-Player::Player(const sf::Texture& t, sf::Vector2f pos)
-	: sprite(sf::Sprite(t))
+Player::Player(const sf::Texture& playerTexture, sf::Vector2f pos, const sf::Texture& shootTexture)
+	: sprite(sf::Sprite(playerTexture)) ,
+	shootSprite(sf::Sprite(shootTexture))
 {
 	sprite.setScale({ 7.f, 7.f });
 	sprite.setPosition(pos);
+
+	shootSprite.setScale({7.f, 7.f});
+	shootSprite.setPosition(getPosition() + sf::Vector2f(25.f, 0.f));
 }
 
 void Player::update(float deltaTime)
@@ -18,11 +22,43 @@ void Player::update(float deltaTime)
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D) && sprite.getPosition().x < WINDOW_WIDTH)
         movement.x += velocity * deltaTime;
 
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space) && activeShoot) {
+		activeShoot = false;
+		setShoot();
+	}
+
+	if (!activeShoot) {
+		shootSprite.move({0, -velocity * deltaTime});
+		if (shootSprite.getPosition().y < 0) {
+			activeShoot = true;
+			shootSprite.setPosition(getPosition() + sf::Vector2f(25.f, 0.f));
+		}
+	}
+
 	sprite.move(movement);
+}
+
+void Player::enemyGetHit() {
+	activeShoot = false;
+}
+
+sf::FloatRect Player::getShootBounds() const {
+	return shootSprite.getGlobalBounds();
+}
+
+bool Player::canShoot() const {
+	return activeShoot;
+}
+
+void Player::setShoot() {
+	shootSprite.setPosition(getPosition() + sf::Vector2f( 25.f, 0.f));
 }
 
 void Player::render(sf::RenderWindow& window)
 {
+	if (!activeShoot) {
+		window.draw(shootSprite);
+	}
 	window.draw(sprite);
 }
 
--- src/Entities/Player.hpp
+++ src/Entities/Player.hpp
@@ -7,7 +7,7 @@
 // Player.hpp
 class Player : public Entity {
 public:
-    Player(const sf::Texture& t, sf::Vector2f pos);
+    Player(const sf::Texture &playerTexture, sf::Vector2f pos, const sf::Texture &shootTexture);
 
     void update(float deltaTime) override;
     void render(sf::RenderWindow& window) override;
@@ -15,7 +15,17 @@ public:
     sf::Vector2f getPosition() const override;
     sf::FloatRect getBounds() const override;
 
+    bool canShoot() const;
+    sf::FloatRect getShootBounds() const;
+
+    void enemyGetHit();
+
 private:
     sf::Sprite sprite;
     float velocity = 400.f;
+
+    bool activeShoot = true;
+    sf::Sprite shootSprite;
+
+    void setShoot();
 };
